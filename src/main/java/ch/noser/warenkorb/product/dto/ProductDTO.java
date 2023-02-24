e0c60d93c0068b0cc448ef47218f22cae01d64ba
Maelwalser <mwa142967@stud.gibb.ch>
2023-02-24 08:06:41 +0100

    Commit day 3

--- src/main/java/ch/noser/warenkorb/product/dto/ProductDTO.java
+++ src/main/java/ch/noser/warenkorb/product/dto/ProductDTO.java
@@ -5,6 +5,22 @@ import java.util.UUID;
 
 public class ProductDTO {
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
     private UUID id;
     private String product_name;
 
